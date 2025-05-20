169b52925e8dead2f3065f085e60f71e3fbd7809
Loic Sharma <sharma.loic@gmail.com>
2025-05-20 15:36:55 -0700

    Tweak

--- main.dart
+++ main.dart
@@ -7,31 +7,29 @@ void main(List<String> arguments) async {
   String? token = Platform.environment['GITHUB_TOKEN'];
   String? after;
 
-  final issues = <Issue>[];
-  for (var i = 0; i < 100; i++) {
-    final response = await loadIssues(token, 'flutter', 'flutter', after);
-
-    issues.addAll(response.issues);
-    after = response.endCursor;
-  }
-
   final outputFile = File('issues.csv').openWrite();
 
   outputFile.writeln('issue_url,issue_created_at,issue_closed_at,open_duration_days,has_associated_pr');
 
-  for (final issue in issues) {
-    final openDuration = issue.closedAt.difference(issue.createdAt);
+  for (var i = 0; i < 1000; i++) {
+    final response = await loadIssues(token, 'flutter', 'flutter', after);
 
-    outputFile.write(issue.url);
-    outputFile.write(',');
-    outputFile.write(issue.createdAt.toUtc().toIso8601String());
-    outputFile.write(',');
-    outputFile.write(issue.closedAt.toUtc().toIso8601String());
-    outputFile.write(',');
-    outputFile.write(openDuration.inDays);
-    outputFile.write(',');
-    outputFile.write(issue.associatedPullRequest != null ? 'true' : 'false');
-    outputFile.writeln();
+    for (final issue in response.issues) {
+      final openDuration = issue.closedAt.difference(issue.createdAt);
+
+      outputFile.write(issue.url);
+      outputFile.write(',');
+      outputFile.write(issue.createdAt.toUtc().toIso8601String());
+      outputFile.write(',');
+      outputFile.write(issue.closedAt.toUtc().toIso8601String());
+      outputFile.write(',');
+      outputFile.write(openDuration.inDays);
+      outputFile.write(',');
+      outputFile.write(issue.associatedPullRequest != null ? 'true' : 'false');
+      outputFile.writeln();
+    }
+
+    after = response.endCursor;
   }
 
   await outputFile.flush();
